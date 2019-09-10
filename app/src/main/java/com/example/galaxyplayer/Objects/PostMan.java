c04ce192bfdc51523bba802c8d4557d6cf71ff83
Farajialireza82 <Farajialireza82@gmail.com>
2019-09-10 18:40:54 +0430

    Copied the PostMan class from the MainActivity to a separate class named "PostMan.java"

---
+++ app/src/main/java/com/example/galaxyplayer/Objects/PostMan.java
@@ -0,0 +1,24 @@
+package com.example.galaxyplayer.Objects;
+
+public class PostMan {
+
+    String status;
+    MusicModel musicModel;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public MusicModel getMusicModel() {
+        return musicModel;
+    }
+
+    public void setMusicModel(MusicModel musicModel) {
+        this.musicModel = musicModel;
+    }
+}
+
