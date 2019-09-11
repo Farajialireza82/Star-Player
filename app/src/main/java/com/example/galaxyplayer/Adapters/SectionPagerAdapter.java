084d1cbbbe3ed445518be5efe0c3339a9a6d7ada
Farajialireza82 <Farajialireza82@gmail.com>
2019-09-11 11:03:40 +0430

    Created a FragmentPagerAdapter

---
+++ app/src/main/java/com/example/galaxyplayer/Adapters/SectionPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.galaxyplayer.Adapters;
+
+import com.example.galaxyplayer.Fragments.LoginFragment;
+import com.example.galaxyplayer.Fragments.PlayFragment;
+import com.example.galaxyplayer.Fragments.SongListFragment;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import static com.example.galaxyplayer.MainActivityClass.LOGIN_FRAGMENT;
+import static com.example.galaxyplayer.MainActivityClass.PLAY_FRAGMENT;
+import static com.example.galaxyplayer.MainActivityClass.SONG_LIST_FRAGMENT;
+
+public class SectionPagerAdapter extends FragmentPagerAdapter {
+
+
+
+    public SectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case SONG_LIST_FRAGMENT:
+                return new SongListFragment();
+            case PLAY_FRAGMENT:
+                return new PlayFragment();
+            case LOGIN_FRAGMENT:
+                return new LoginFragment();
+            default:
+                return null;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+}
