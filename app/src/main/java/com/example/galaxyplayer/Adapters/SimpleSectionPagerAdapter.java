fbdece3a87a4cd1a7e371bbfad8a0f66dd1d2cff
Farajialireza82 <Farajialireza82@gmail.com>
2019-09-12 12:15:33 +0430

    Added a add Fragment () and getPageTitle() method to the SimpleSectionPagerAdapter class

--- app/src/main/java/com/example/galaxyplayer/Adapters/SimpleSectionPagerAdapter.java
+++ app/src/main/java/com/example/galaxyplayer/Adapters/SimpleSectionPagerAdapter.java
@@ -5,15 +5,22 @@ import com.example.galaxyplayer.Fragments.LoginFragment;
 import com.example.galaxyplayer.Fragments.PlayFragment;
 import com.example.galaxyplayer.Fragments.SongListFragment;
 
+import java.util.ArrayList;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 import static com.example.galaxyplayer.Activities.MainActivityClass.LOGIN_FRAGMENT;
-import static com.example.galaxyplayer.Activities.MainActivityClass.PLAY_FRAGMENT;
 import static com.example.galaxyplayer.Activities.MainActivityClass.SONG_LIST_FRAGMENT;
 
 public class SimpleSectionPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList <Fragment> fragments = new ArrayList<>();
+    ArrayList < String > fragmentsTitle = new ArrayList<>();
+
+
 
     public SimpleSectionPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -21,20 +28,25 @@ public class SimpleSectionPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        switch (position) {
-            case LOGIN_FRAGMENT:
-                return new LoginFragment();
-            case PLAY_FRAGMENT:
-                return new PlayFragment();
-            case SONG_LIST_FRAGMENT:
-                return new SongListFragment();
-            default:
-                return null;
-        }
+
+        return fragments.get(position);
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return fragments.size();
+    }
+
+    public void addFragment(Fragment fragment , String fragmentTitle){
+
+        fragments.add(fragment);
+        fragmentsTitle.add(fragmentTitle);
+
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentsTitle.get(position);
     }
 }
