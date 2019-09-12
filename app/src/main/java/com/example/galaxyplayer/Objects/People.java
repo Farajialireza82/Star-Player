6ef6171bd85990df86aeedacf5aa4dc396eb707e
Farajialireza82 <Farajialireza82@gmail.com>
2019-09-12 16:18:50 +0430

    Created a People class for Getting result from swapi with Retrofit

---
+++ app/src/main/java/com/example/galaxyplayer/Objects/People.java
@@ -0,0 +1,12 @@
+package com.example.galaxyplayer.Objects;
+
+import java.util.List;
+
+public class People {
+
+    List<Person> results ;
+
+    public List<Person> getResults() {
+        return results;
+    }
+}
