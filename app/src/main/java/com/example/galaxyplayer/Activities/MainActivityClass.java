dbe86ebf36b37fa9b373a9b251cbc00245a05ac2
Farajialireza82 <Farajialireza82@gmail.com>
2019-09-12 16:13:03 +0430

    Added TabLayout to the MainActivityClass

--- app/src/main/java/com/example/galaxyplayer/Activities/MainActivityClass.java
+++ app/src/main/java/com/example/galaxyplayer/Activities/MainActivityClass.java
@@ -12,7 +12,12 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.galaxyplayer.Adapters.SimpleSectionPagerAdapter;
+import com.example.galaxyplayer.Fragments.LoginFragment;
+import com.example.galaxyplayer.Fragments.MusicPlayerFragment;
+import com.example.galaxyplayer.Fragments.SongListFragment;
+import com.example.galaxyplayer.Fragments.SwapiFragment;
 import com.example.galaxyplayer.R;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,22 +27,23 @@ import androidx.viewpager.widget.ViewPager;
 
 public class MainActivityClass extends AppCompatActivity {
 
-        static ViewPager viewPager;
-        public SimpleSectionPagerAdapter simpleSectionPagerAdapter;
+    static ViewPager viewPager;
+    public SimpleSectionPagerAdapter simpleSectionPagerAdapter;
+    TabLayout tabLayout;
 
-        private static final String TAG = "MainActivityClass";
+    private static final String TAG = "MainActivityClass";
 
-        public static final int SONG_LIST_FRAGMENT = 2;
-        public static final int LOGIN_FRAGMENT = 0;
-        public static final int PLAY_FRAGMENT = 1;
+    public static final int SWAPI_FRAGMENT = 1;
+    public static final int MUSIC_PLAYER_FRAGMENT = 0;
 
-        public static boolean permissionAllowed;
+    public static boolean permissionAllowed;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_layout);
 
+        tabLayout = findViewById(R.id.tabLayout_id);
 
         viewPager = findViewById(R.id.view_pager);
 
@@ -49,18 +55,16 @@ public class MainActivityClass extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: sectionPageAdapter set to viewPager");
 
+        simpleSectionPagerAdapter.addFragment(new MusicPlayerFragment(), "Music Player");
+        simpleSectionPagerAdapter.addFragment(new SwapiFragment(), "Swapi");
+
+        simpleSectionPagerAdapter.notifyDataSetChanged();
+
+        tabLayout.setupWithViewPager(viewPager);
 
         Log.d(TAG, "onCreate: Opening Login Fragment");
 
-        goToFragment(LOGIN_FRAGMENT);
-
         permissionAllowed = checkPermissionREAD_EXTERNAL_STORAGE(this);
-
-    }
-
-    public static void goToFragment(int fragment) {
-
-        viewPager.setCurrentItem(fragment);
 
     }
 
