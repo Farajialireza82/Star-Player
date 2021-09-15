843b910eafd14a3efe7191bd0bc88ade58303025
Alireza-Faraji <farajialireza82@gmail.com>
2021-09-15 16:55:21 +0430

    Main Activity to Kotlin

---
+++ app/src/main/java/com/example/galaxyplayerkotlin/Activities/MainActivityClass.kt
@@ -0,0 +1,7 @@
+package com.example.galaxyplayerkotlin.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.galaxyplayerkotlin.R
+
+class MainActivityClass : AppCompatActivity(R.layout.activity_main_layout)
