98f0ce5996568a9650d02a3a106c66cc6bc8bdb6
Alireza-Faraji <farajialireza82@gmail.com>
2021-09-24 16:14:29 +0330

    Reduced view pager sensitivity

--- app/src/main/java/com/example/galaxyplayerkotlin/Activities/MainActivityClass.kt
+++ app/src/main/java/com/example/galaxyplayerkotlin/Activities/MainActivityClass.kt
@@ -3,6 +3,7 @@ package com.example.galaxyplayerkotlin.Activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.galaxyplayerkotlin.Adapters.ViewPagerAdapter
 import com.example.galaxyplayerkotlin.R
@@ -28,7 +29,26 @@ class MainActivityClass : AppCompatActivity(R.layout.activity_main_layout) {
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = fragmentNames[position]
         }.attach()
+
+        viewPager.reduceDragSensitivity()
+
+
+
     }
+
+    private fun ViewPager2.reduceDragSensitivity() {
+        val recyclerViewField = ViewPager2::class.java.getDeclaredField("mRecyclerView")
+        recyclerViewField.isAccessible = true
+        val recyclerView = recyclerViewField.get(this) as RecyclerView
+
+        val touchSlopField = RecyclerView::class.java.getDeclaredField("mTouchSlop")
+        touchSlopField.isAccessible = true
+        val touchSlop = touchSlopField.get(recyclerView) as Int
+        touchSlopField.set(recyclerView, touchSlop*5)       // "8" was obtained experimentally
+    }
+
+
+
 
     val fragmentNames = arrayOf(
         "Tracks",
